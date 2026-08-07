04fc4becae4b4d01963bf22c1d14c82bd8c9451e
Nipun746 <sachdevanipun28@gmail.com>
2026-08-07 14:08:47 +0530

    Time: 16 ms (59.08%), Space: 12.3 MB (56.79%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -6,26 +6,12 @@ class Solution(object):
         :rtype: str
         """
         final = ""
-        
-        if len(word1) == len(word2):
-            for char1,char2 in zip(word1,word2):
-                final = final + char1
-                final = final + char2
-            return final
-        elif len(word1)<len(word2):
-            
-                n = len(word1)
-                wordx2 = word2[:n]
-                for char1,char2 in zip(word1,wordx2):
-                    final = final + char1
-                    final = final + char2
-                final = final + word2[n:]
-                return final
-        elif len(word1)>len(word2):
-                n = len(word2)
-                wordx1 = word1[:n]
-                for char1,char2 in zip(wordx1,word2):
-                    final = final + char1
-                    final = final + char2
-                final = final + word1[n:]
-                return final
+        for char1,char2 in zip(word1,word2):
+                final = final + char1 + char2
+        if len(word1)>len(word2):
+            n = len(word2)
+            final = final + word1[n:]
+        if len(word2)>len(word1):
+            n = len(word1)
+            final = final + word2[n:]
+        return final
