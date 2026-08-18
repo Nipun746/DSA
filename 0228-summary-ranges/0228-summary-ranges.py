796008376fa363dacf3252766aad35db5e004813
Nipun746 <sachdevanipun28@gmail.com>
2026-08-18 19:10:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.12%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -6,18 +6,17 @@ class Solution(object):
         """
         ans = []
         i = 0
-
         while i < len(nums):
             start = nums[i]
 
-            while i < len(nums)-1 and nums[i] + 1 == nums[i+1]:
-                i += 1
-
+            while i<len(nums)-1 and nums[i] + 1 == nums[i+1]:
+                i+=1
+            
             if start != nums[i]:
-                ans.append(str(start) + '->' + str(nums[i]))
+                ans.append(str(start) + "->" + str(nums[i]))
+
             else:
-                ans.append(str(nums[i]))
+                ans.append(str(start))
 
-            i += 1 
-
+            i+=1
         return ans
